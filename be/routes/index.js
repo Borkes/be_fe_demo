40eb831e2acb81062f88bf4377683da067a56449
Borkes <1043568089@qq.com>
2017-06-14 19:22:31 +0800

    自动路由

--- be/routes/index.js
+++ be/routes/index.js
@@ -1,8 +1,21 @@
-module.exports = function (app) {
+// 传入app路由
+/*module.exports = function (app) {
   app.get('/', function (req, res) {
     res.redirect('/index');
   });
   app.get('/hello', function(req, res) {
     res.redirect('/hello');
   })
-};
+};*/
+//自动路由
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function (req, res) {
+    res.redirect('/index');
+})
+router.get('/hello', function(req, res) {
+  res.redirect('/hello');
+})
+
+module.exports = router;
