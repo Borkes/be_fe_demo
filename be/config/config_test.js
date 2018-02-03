81f40d0ee14f10a0ce21413942c4b134625723fb
Borkes <1043568089@qq.com>
2018-02-03 11:48:59 +0800

    mongod

--- be/config/config_test.js
+++ be/config/config_test.js
@@ -1,6 +1,6 @@
 'use strict'
 var config = {};
-config.redisConfig = {
+config.redisConfig = {    //redis 配置
     host: '127.0.0.1',
     port: 6379,
     opt: {
@@ -8,15 +8,19 @@ config.redisConfig = {
     }
 }
 
-config.dbConfig = {
+config.dbConfig = {  //mysql数据库配置
     host: '127.0.0.1',
     port: 3306,
     user: 'root',
     password: '123456',
     database: 'myblog',
     connectTimeout: 5000,
-    connectionLimit : 5,
-    waitForConnctions : true
+    connectionLimit: 5,
+    waitForConnctions: true
+}
+
+config.mongoConfig = {
+    url: 'mongodb://localhost/mhq',
 }
 module.exports = config;
 //exports.config = config
