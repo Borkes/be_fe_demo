d9443521888b478c05da31e5a4afc41474a269e8
Borkes <1043568089@qq.com>
2018-02-03 12:30:35 +0800

    格式化

--- be/app.js
+++ be/app.js
@@ -18,12 +18,12 @@ app.use(cookieParser()); //使用cookie
 app.set('views', path.join(__dirname, 'views'));
 // 设置模板引擎为 ejs
 app.set('view engine', 'ejs');
-app.set('routes', path.join(__dirname, 'routes')) 
+app.set('routes', path.join(__dirname, 'routes'))
 app.set('title', 'my app example') //默认settings, view里面使用settings.title
 // 设置静态文件目录
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true})); //for parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true })); //for parsing application/x-www-form-urlencoded
 app.use(log4js.connectLogger(log4js.getLogger('main'), { level: 'AUTO', format: ':remote-addr :method :url :status :response-time ms' }))
 // session 中间件
 app.use(middle.session(redisStore));
@@ -31,27 +31,27 @@ app.use(middle.session(redisStore));
 // 自动路由
 const routers = app.get('routes');
 function runRouter(root, routers) {
-  fs.readdirSync(routers).forEach(function (fileName) {
-    let filePath = routers + '/' + fileName;
-    let rname = fileName.substr(0, fileName.lastIndexOf("."));
-    //console.log(1,rname,2, fileName,3, filePath)
-    if (!fs.lstatSync(filePath).isDirectory()) {
-      if (rname === 'index') {
-        app.use(root + '/', require(filePath));
-      }
-      app.use(root + '/' + rname, require(filePath));
-    } else {
-      let path = root + '/' + fileName;
-      runRouter(path, filePath);
-    }
-  })
+    fs.readdirSync(routers).forEach(function (fileName) {
+        let filePath = routers + '/' + fileName;
+        let rname = fileName.substr(0, fileName.lastIndexOf("."));
+        //console.log(1,rname,2, fileName,3, filePath)
+        if (!fs.lstatSync(filePath).isDirectory()) {
+            if (rname === 'index') {
+                app.use(root + '/', require(filePath));
+            }
+            app.use(root + '/' + rname, require(filePath));
+        } else {
+            let path = root + '/' + fileName;
+            runRouter(path, filePath);
+        }
+    })
 }
 runRouter('', routers);
 
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  res.send(err.message)
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    res.send(err.message)
 })
 
 // 监听端口，启动程序
@@ -67,11 +67,11 @@ app.listen = function(...arg) {
 */
 
 process.on('uncatchException', (err) => {
-  console.log(err);
+    console.log(err);
 })
 
-http.createServer(app).listen(8088, function() {
-  console.log('hello world http 8088')
+http.createServer(app).listen(8088, function () {
+    console.log('hello world http 8088')
 });
 //https 没有安全证书
 /*const option = {
--- be/routes/index.js
+++ be/routes/index.js
@@ -19,52 +19,52 @@ router.get('/', function (req, res) {
 })
 
 //登录
-router.post('/login', function(req, res) {
-  let name = req.body.name.trim();
-  let password = req.body.password.trim();
-  if (!name || !password) {
-    return res.send({code: 1, message: '请输入帐号和密码'});
-  }
-  
-  userAct.login(name).then((result) => {
-    if (result.length === 0) {
-      return res.send({code: 0, message: '帐号未注册'});
-    } else if (result[0].password === password) {
-      return res.send({code: 0, message: '登录成功'});
-    } else {
-      return res.send({code: 1, message: '密码错误'});
+router.post('/login', function (req, res) {
+    let name = req.body.name.trim();
+    let password = req.body.password.trim();
+    if (!name || !password) {
+        return res.send({ code: 1, message: '请输入帐号和密码' });
     }
-  }).catch((err) => {
-    console.error('login error:', err);
-    res.send({code: 1, message: '登录失败'});
-  })
+
+    userAct.login(name).then((result) => {
+        if (result.length === 0) {
+            return res.send({ code: 0, message: '帐号未注册' });
+        } else if (result[0].password === password) {
+            return res.send({ code: 0, message: '登录成功' });
+        } else {
+            return res.send({ code: 1, message: '密码错误' });
+        }
+    }).catch((err) => {
+        console.error('login error:', err);
+        res.send({ code: 1, message: '登录失败' });
+    })
 
 })
 
 //注册,应该还有其他考虑,注册帐号频率,ip频率
-router.post('/register', function(req, res) {
-  let name = req.body.name;
-  let password = req.body.password; //加密
-  if (!name || !password) {
-    return res.send({code: 2, message: '没有必要参数'})
-  }
-  let info = {name, password};
-  userAct.existAccount(name).then((exist) => {
-    if (exist) {
-      return 1;
-    } else {
-      return userAct.register(info);
+router.post('/register', function (req, res) {
+    let name = req.body.name;
+    let password = req.body.password; //加密
+    if (!name || !password) {
+        return res.send({ code: 2, message: '没有必要参数' })
     }
-  }).then((data) => {
-    if (data === 1) {
-      return res.send({code: 0, message: '帐号已经被注册'});
-    } else {
-      return res.send({code: 0, message: '注册成功'});
-    }
-  }).catch((err) => {
-    console.error('register error:', err);
-    return res.send({code: 1, message: '注册失败'});
-  })
+    let info = { name, password };
+    userAct.existAccount(name).then((exist) => {
+        if (exist) {
+            return 1;
+        } else {
+            return userAct.register(info);
+        }
+    }).then((data) => {
+        if (data === 1) {
+            return res.send({ code: 0, message: '帐号已经被注册' });
+        } else {
+            return res.send({ code: 0, message: '注册成功' });
+        }
+    }).catch((err) => {
+        console.error('register error:', err);
+        return res.send({ code: 1, message: '注册失败' });
+    })
 })
 
 module.exports = router;
